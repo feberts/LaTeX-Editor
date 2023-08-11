78ca5c967ad0ccb712c574bb3a1c53e7fb4f6a05
github action <noreply>
2023-08-11 20:58:42 +0000

    update

--- docs/Folien/config.js
+++ docs/Folien/config.js
@@ -1,4 +1,4 @@
-const template_name = "Vorlage für Folien";
+const template_name = "Folien";
 const main_tex_file = "Folien Präsentation.tex";
 const files = ["Abbildung.png","Folien Präsentation.tex","Mainmatter.tex","Quellen.bib",];
 const placeholders = ["Vorname","Nachname",];
