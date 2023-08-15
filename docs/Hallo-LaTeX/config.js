936e4b8c04df0674f3589dfd0da72424156b660d
github action <noreply>
2023-08-15 16:48:28 +0000

    update

--- docs/Hallo-LaTeX/config.js
+++ docs/Hallo-LaTeX/config.js
@@ -1,4 +1,4 @@
-const template_name = "Hallo-LaTeX";
-const main_tex_file = "hello_latex.tex";
-const files = ["einstein.jpg","hello_latex.tex","sonstiges.tex",];
-const placeholders = ["Titel","Autor","Text ",];
+const config_template_name = "Hallo-LaTeX";
+const config_main_tex_file = "hello_latex.tex";
+const config_project_files = ["einstein.jpg","hello_latex.tex","literatur.bib","sonstiges.tex",];
+const config_placeholders = ["Titel","Autor","Ergänzender Text ",];
