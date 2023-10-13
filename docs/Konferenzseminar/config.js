203875f6eabdd50872ec4ac12522350e261f5894
github action <noreply>
2023-10-13 15:37:57 +0000

    update

--- docs/Konferenzseminar/config.js
+++ docs/Konferenzseminar/config.js
@@ -1,4 +1,4 @@
 var config_template_name = "Konferenzseminar";
-var config_main_tex_file = "Seminararbeit.tex";
-var config_project_files = ["IEEEtran.cls","Seminararbeit.tex","deeplearning.png","literatur.bib",];
-var config_placeholders = ["Titel der __ Seminararbeit","Max Mustermann",];
+var config_main_tex_file = "IEEEtran.cls";
+var config_project_files = ["IEEEtran.cls","Seminararbeit.pdf","Seminararbeit.tex","deeplearning.png","literatur.bib",];
+var config_placeholders = ["cd","g","cd","_normalfont _normalcolor (_theequation)","_normalfont _normalcolor (_theIEEEsubequation)","c","c","ab","de","ab","de","cd","g","cd","#1","_@@IEEEauthorhaligncr","0pt","1em","#1","-2ex",];
